347aa32add98be3e17c57303454166e7a46c7e5a
Francesco <you@example.com>
2025-02-25 12:45:12 +0100

    Add snack 8

--- js/main.js
+++ js/main.js
@@ -114,3 +114,24 @@ function eseguiEferma(messaggio, avvio, stop) {
 }
 
 // eseguiEferma("Messaggio stampato", 1000, 10000);
+
+//! Snack 8 (Bonus)
+/* Crea una funzione che simula un conto alla rovescia
+
+    Scrivi una funzione contoAllaRovescia che accetta un numero n e stampa il conto alla rovescia da n a 0, con un intervallo di 1 secondo tra ogni numero. Quando arriva a 0, stampa "Tempo scaduto!" e interrompe il timer.*/
+
+function contoAllaRovescia(num) {
+  let count = num;
+  return function () {
+    const startPrint = setInterval(() => {
+      count--, console.log(count);
+    }, 1000);
+    setTimeout(
+      () => clearInterval(startPrint, console.log("Tempo scaduto!")),
+      10000
+    );
+  };
+}
+const countdown = contoAllaRovescia(10);
+
+countdown();
